1f9951ba0ee077456b436f99bb7381b975ccc4b6
Mimzi3 <jaymimisu@gmail.com>
2023-03-23 13:38:36 -0700

    7diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: parameter
+ * Return: returns nothing
+ */
+
+void print_diagonal(int n)
+{
+	while (n\\ > 0)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
