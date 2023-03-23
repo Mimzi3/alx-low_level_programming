814e38e0c256c41640a39528ecbcfda621e3855f
Mimzi3 <jaymimisu@gmail.com>
2023-03-23 12:43:01 -0700

    threeprint

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
+ * Return: The numbers since 0 up to 9
  */
 
-void pritn_numbers(void)
+void print_numbers(void)
 {
-	int i;
+	int c;
 
-	for (i = 0; i < 10; i++)
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar(i + '0');
+		_putchar(c + '0');
 	}
-	_putchar('/n');
+	_putchar('\n');
 }
