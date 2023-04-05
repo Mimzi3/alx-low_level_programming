6c85b5a776e72d49001abe2fa9cca8f16e2ebfbf
Mimzi3 <jaymimisu@gmail.com>
2023-04-05 11:19:28 -0700

    6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * checker - checks recursively the input from is_prime_number
- * @n: iterator
- * @base: base number to check
- * Return: 1 if n is a prime, else return 0 otherwise.
+ * find_multipliers - look for multipliers of n.
+ * @n: base number.
+ * @i: iterator number.
+ * Return: 1 if n is prime, 0 otherwise.
  */
-
 int find_multipliers(int n, int i)
 {
 	if (i == n)
@@ -22,7 +21,6 @@ int find_multipliers(int n, int i)
  *
  * Return: 1 if n is prime, 0 otherwise.
  */
-
 int is_prime_number(int n)
 {
 	if (n <= 1)
